3edb68db2cbde23862fe94f253b2ff0f538535c2
LEARN01 <learn01@LEARN01.local>
2015-01-19 10:19:42 -0800

    added click functionality to tardis animation

--- js/animation1.js
+++ js/animation1.js
@@ -1,95 +1,27 @@
-
-//   if (ship.style["left"]  === "600px") {
-//     ship.style["left"] = "0px";
-//   } else {
-//     ship.style["left"] = "600px";
-//   }
-// }
-
-
 $(document).ready( function () {
 
-  // var shipj = $('#spaceship');
-  // var ship = document.getElementById("spaceship");
-  // ship.style["left"] = "100px";
-  // ship.style["top"] = "100px";
-  // shipj.addClass("spin-bkw");
-  //
-  // setInterval(moveToPlace, 3000);
-  //
-  // var increment = 0;
-  // var loc = [ [125,125],
-  //             [150,150],
-  //             [175,175],
-  //             [200,200],
-  //             [175,225],
-  //             [150,250],
-  //             [100,300],
-  //             [ 50,250],
-  //             [  0,200],
-  //             [ 50,150],
-  //             [100,100],
-  //             ]
+  var shipj = $('#spaceship');
+  var space = $('#space');
+  var ship = document.getElementById("spaceship");
+  var timerId;
+  var spacePad = 4;
 
+  function shrink() {
+    if ( shipj.height() < 30) {
+      shipj.hide();
+      clearInterval(timerId);
+    } else {
+      shipj.animate({
+        height: '-=' + shipj.height() / 2,
+        width: '-=' + shipj.width() / 2
+      });
+      space.css("padding", ( (spacePad + 80) + "px") );
+    }
+  }
 
-  // function moveToPlace() {
-    //el.style["z-index"] = "1000";
-    // if ( shipj.hasClass("spin-fwd") ) {
-    //   shipj.removeClass("spin-fwd")
-    //   shipj.addClass("spin-bkw");
-    // } else {
-    //   shipj.removeClass("spin-bkw")
-    //   shipj.addClass("spin-fwd");
-    // }
+  shipj.click( function() {
+    //shrink();
+    timerId = setInterval(shrink, 3000);
+  });
 
-  //   switch ( true ) {
-  //     case (increment === 0) :
-  //       ship.style["left"] = "150px";
-  //       ship.style["top"] = "150px";
-  //       increment++;
-  //       break;
-  //
-  //     case (increment === 1) :
-  //       ship.style["left"] = "200px";
-  //       ship.style["top"] = "200px";
-  //       increment++;
-  //       break;
-  //
-  //     case (increment === 2) :
-  //       ship.style["left"] = "150px";
-  //       ship.style["top"] = "250px";
-  //       increment++;
-  //       break;
-  //
-  //     case (increment === 3) :
-  //       ship.style["left"] = "100px";
-  //       ship.style["top"] = "300px";
-  //       increment++;
-  //       break;
-  //
-  //     case (increment === 4) :
-  //       ship.style["left"] = "50px";
-  //       ship.style["top"] = "250px";
-  //       increment++;
-  //       break;
-  //
-  //     case (increment === 5) :
-  //       ship.style["left"] = "0px";
-  //       ship.style["top"] = "200px";
-  //       increment++;
-  //       break;
-  //
-  //     case (increment === 6) :
-  //       ship.style["left"] = "50px";
-  //       ship.style["top"] = "150px";
-  //       increment++;
-  //       break;
-  //
-  //     case (increment === 7) :
-  //       ship.style["left"] = "100px";
-  //       ship.style["top"] = "100px";
-  //       increment=0;
-  //
-  //   };
-  // };
 })
