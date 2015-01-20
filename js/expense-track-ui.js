8a90c7780ab3aed52d2fa34394e47d3978b66e07
LEARN01 <learn01@LEARN01.local>
2015-01-20 11:14:45 -0800

    complete expense tracking with multiple categories

--- js/expense-track-ui.js
+++ js/expense-track-ui.js
@@ -22,21 +22,59 @@ $(document).ready(function() {
 
     // create the new category object
     var newCategory = new expenseCategory( categoryDesc.val() );
+    categoryDesc.val("");
 
     // add the link to the list
-    $(".categories").append("<p class=\"category\">" + categoryDesc.val() + "</p>"  );
+    $(".categories").append("<p id=\"category" + newCategory.identity + "\">" + newCategory.desc + "</p>" );
+    var categoryLink = $("#category" + newCategory.identity);
+
+    // change the heading to show the new category as the current category
+    //   that items can be added to
+    $("#current-category").text(newCategory.desc);
 
     // create the new detail expenses table and display it
     var newCategoryTable = $("table.template").clone();
-    newCategoryTable.attr("id", "table-category" + newCategory.identity );
-    $(".template").hide();
-    $("#table-category" + newCategory.identity ).show();
+    newCategoryTable.attr("id", "expense-table" + newCategory.identity );
+    newCategoryTable.removeClass("template");
+    $(".expense-tables").hide();
+
+    // add the new detail expense table to the DOM and display it
+    $(".expenses-column").append(newCategoryTable);
+    newCategoryTable.show();
+
+    // give the "grand total" a unique Id so we can reference it later
+    var grandTotalSpan = newCategoryTable.children("tfoot").find("span");
+    grandTotalSpan.attr("id", "grandtotal" + newCategory.identity );
 
     // create the "add expense" button specific to this category and display it
-    var newButton = $(".add-expense").clone();
+    var newButton = $("button.template").clone();
     newButton.attr("id", "add-expense"+ newCategory.identity );
-
+    newButton.removeClass("template");
+    $(".expense-buttons").append(newButton);
+    $(".expense-button").hide();
     newButton.show();
+
+
+    /*
+    ======================================================
+     Set all the events for the new DOM items
+    ======================================================
+    */
+
+    categoryLink.click( function () {
+      // hide all the currently active add buttons
+      $(".expense-button").hide();
+
+      // hide all the currently active detail tables
+      $(".expense-tables").hide();
+
+      // $("#add-expense" + $(this).attr("id").slice(-1) ).show();
+      // $("#expense-table" + $(this).attr("id").slice(-1) ).show();
+      $("#add-expense" + newCategory.identity ).show();
+      $("#expense-table" + newCategory.identity ).show();
+      $("#current-category").text(newCategory.desc);
+
+    });
 
     // set the event for when the user adds expenses to this category
     newButton.click( function () {
@@ -56,7 +94,7 @@ $(document).ready(function() {
 
       newCategory.expenses.push( newExpense );
 
-      $("#expense-table" + newCategory.identity + " > tbody:last").append("<tr class=\"table-striped\">" +
+      $("#expense-table" + newCategory.identity + " > tbody:last").append("<tr>" +
       "<td>" + newExpense.desc +  "</td>" +
       "<td>" + newExpense.quantity.toFixed(0) +  "</td>" +
       "<td>" + newExpense.unitPrice.toFixed(2) +  "</td>" +
@@ -67,7 +105,7 @@ $(document).ready(function() {
         grandTotal += expense.totalPrice;
       });
 
-      $("#grandtotal").text(grandTotal.toFixed(2));
+      $("#grandtotal" + newCategory.identity).text(grandTotal.toFixed(2));
 
       price.val("");
       qty.val("");
@@ -78,39 +116,6 @@ $(document).ready(function() {
 
     event.preventDefault();
 
-  })
-
-
-  // handle adding expenses
-  form.submit(function(event) {
-
-    if ( !( $.isNumeric(qty.val()) ) ) {
-      alert("Quantity must be a valid number.");
-      return false;
-    }
-
-    if ( !($.isNumeric(price.val()) ) ) {
-      alert("Price must be a valid number.");
-      return false;
-    }
-
-    var newExpense = new expense( desc.val(), parseInt(qty.val()), parseFloat(price.val()) );
-    newExpense.getTotalPrice();
-
-    $("#expense-table > tbody:last").append("<tr class=\"table-striped\">" +
-                      "<td>" + newExpense.desc +  "</td>" +
-                      "<td>" + newExpense.quantity.toFixed(0) +  "</td>" +
-                      "<td>" + newExpense.unitPrice.toFixed(2) +  "</td>" +
-                      "<td>" + newExpense.totalPrice.toFixed(2) +  "</td></tr>");
-
-    grandTotal += newExpense.totalPrice;
-    $("#grandtotal").text(grandTotal.toFixed(2));
-
-    price.val("");
-    qty.val("");
-    desc.val("");
-
-    event.preventDefault();
   });
 
 });
